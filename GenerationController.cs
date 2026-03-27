1f439318d4c73d447322ef360655afad1b35c225
FR <137614590+FilipRuman@users.noreply.github.com>
2026-03-27 14:06:45 +0100

    changed the ground generation architecutre

--- GenerationController.cs
+++ GenerationController.cs
@@ -8,13 +8,16 @@ public partial class GenerationController : Node
         [Export] GroundMeshGen ground_mesh_gen;
         [Export] BiomeGenerator biome_generator;
         [Export] int ground_mesh_resolution;
+        [Export] MeshInstance3D mesh_instance;
+        [Export] CollisionShape3D collider;
         [Export] Biome[] biomes;
         [Export] GroundShaderController ground_shader_controller;
         private void Run()
         {
+
                 Vector2I base_world_pos = new(0, 0);
-                ground_mesh_gen.GenerateChunkData(ground_mesh_resolution, terrain_chunk_size, base_world_pos);
-                ground_mesh_gen.ApplyData();
+                var mesh_data = ground_mesh_gen.GenerateChunkData(ground_mesh_resolution, terrain_chunk_size, base_world_pos);
+                ground_mesh_gen.ApplyData(mesh_data, mesh_instance, collider);
                 var biome_data = biome_generator.GenerateTextureData(base_world_pos, terrain_chunk_size, biomes);
                 ground_shader_controller.SetShaderConfiguration(biomes,
                 [biome_data.GetTexture(0)],
@@ -24,3 +27,4 @@ public partial class GenerationController : Node
         }
 
 }
+
